0678879b465a9c661ef17fbc1563c648d0023f48
Fuxh <fuxihuo0812@qq.com>
2020-07-01 07:27:42 +0000

    用户显示页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('user.show', compact('user'));
+    }
+}
