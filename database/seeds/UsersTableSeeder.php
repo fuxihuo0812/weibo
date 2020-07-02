f21f66b602f3f37aad93fb3434d643961fb8a0c6
Fuxh <fuxihuo0812@qq.com>
2020-07-02 02:00:26 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '憨憨兽';
         $user->email = 'fuxihuo0812@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
